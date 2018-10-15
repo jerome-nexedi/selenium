52c817e7530377d205895df4e0b86e6d2b4efbf4
Jérome Perrin <jerome@nexedi.com>
2018-10-15 15:26:32 +0200

    XXX workaround travis vs github API ( not for merge )

--- scripts/travis/install.sh
+++ scripts/travis/install.sh
@@ -12,6 +12,7 @@ if [[ ! -z $MARIONETTE ]]; then
     export GITHUB_AUTH="token $GITHUB_TOKEN"
   fi
   export GECKODRIVER_DOWNLOAD=`curl -H "Authorization: $GITHUB_AUTH" -s 'https://api.github.com/repos/mozilla/geckodriver/releases/latest' | python -c "import sys, json; r = json.load(sys.stdin); print([a for a in r['assets'] if 'linux64' in a['name']][0]['browser_download_url']);"`
+  export GECKODRIVER_DOWNLOAD=https://github.com/mozilla/geckodriver/releases/download/v0.23.0/geckodriver-v0.23.0-linux64.tar.gz
   curl -L -o geckodriver.tar.gz $GECKODRIVER_DOWNLOAD
   gunzip -c geckodriver.tar.gz | tar xopf -
   chmod +x geckodriver && sudo mv geckodriver /usr/local/bin
